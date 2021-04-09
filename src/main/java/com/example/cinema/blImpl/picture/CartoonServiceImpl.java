45be225cda5f9d903f3dfa334fe1e0c783203cd2
wangqi <1209017023@qq.com>
2021-04-09 14:59:19 +0800

    解决了ps必须使用本地图片路径的问题，现在图片可以直接从项目里面读取

--- src/main/java/com/example/cinema/blImpl/picture/CartoonServiceImpl.java
+++ src/main/java/com/example/cinema/blImpl/picture/CartoonServiceImpl.java
@@ -18,21 +18,15 @@ public class CartoonServiceImpl implements CartoonService {
     @Override
     public ResponseVO ps(String path_real,String path_cartoon){
 //
-//        String property_1 = System.getProperty("user.dir");
-//        String savePath = property_1 + "\\src\\main\\resources\\static\\photos\\";
+        String property_1 = System.getProperty("user.dir");
+        String savePath = property_1 + "\\src\\main\\resources\\static\\photos\\";
 
         //String savePath = "D:\\new\\";//图片存放位置
         // 参数说明
         // type ： opencv和baidu 两种获取人脸标记的位置点
         // jingxi : true 使用全部点位进行分割，false使用外部轮廓的点位进行融合
 
-         path_real = "D:\\new\\ertong1.jpg";
-         //path_cartoon= "D:\\new\\11.jpg";
-        String savePath = "D:\\new\\";//图片存放位置
-        String property = System.getProperty("user.dir");
-         path_cartoon = property + "\\src\\main\\resources\\static\\photos\\gongzhu.jpg";
 
-         System.out.println(path_cartoon);
         openCVFaceSwap.faceMerge(path_real,path_cartoon,savePath,"opencv",false);
 
         return ResponseVO.buildSuccess();
