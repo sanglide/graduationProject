e787b9d5c601ae448ac6cb37f68bf2ecb574fd8a
17125 <171250022@smail.nju.edu.cn>
2021-04-14 11:00:59 +0800

    "语音大眼睛小嘴巴"

--- src/main/java/com/example/cinema/blImpl/talking/ConvertImageContent.java
+++ src/main/java/com/example/cinema/blImpl/talking/ConvertImageContent.java
@@ -5,20 +5,29 @@ import com.example.cinema.po.ImageContent;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class ConvertImageContent {
     /**这个对象将一个字符串的list转换为imageContent对象*/
     List<String> info;
+    Map<String,String> image;
     public void setInfo(List<String> info){
         this.info=info;
     }
-    public ImageContent convertImage(){
+    public void convertImage(){
         ImageContent imageContent=new ImageContent();
-        //todo:
-        return imageContent;
+        //将本对象的info这个list挨个查找，
+        // 并设置image Content的内容，最终形成一个能用的imageContent
+        for(int i=0;i<info.size();i++){
+            String temp=info.get(i);
+            //todo:查找数据库里面有没有temp这个图片，
+            // 如果没有，跳过；如果有，获取它的类型。存进相应的类型中，并存储图片所在哈希值
+
+        }
     }
 
     public void main(String[] args){
+        /**这个函数就是输入一个字符串，生成一张图片*/
         List<String> info1=new ArrayList<String>(Arrays.asList("o1", "o2"));
         List<String> info2=new ArrayList<String>(Arrays.asList("o1", "o2"));
         List<String> info3=new ArrayList<String>(Arrays.asList("o1", "o2"));
@@ -30,7 +39,6 @@ public class ConvertImageContent {
 
         ConvertImageContent convertImageContent=new ConvertImageContent();
         convertImageContent.setInfo(info1);
-        ImageContent imageContent=convertImageContent.convertImage();
-        imageContent.printContent();
+        convertImageContent.convertImage();
     }
 }
